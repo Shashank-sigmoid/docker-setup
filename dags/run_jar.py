d3c5079192fb193726bc424207ab80a43065eaa3
shantanu-sigmoid <98578520+shantanu-sigmoid@users.noreply.github.com>
2022-05-16 12:33:16 +0530

    jar bash command updated

--- dags/run_jar.py
+++ dags/run_jar.py
@@ -3,13 +3,21 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime,timedelta
 
-with DAG('aaaaaASCII', description='Hello World DAG',
-  schedule_interval='* * * * *',
-  start_date=datetime(2022, 3, 20), catchup=False) as dag:
+with DAG('JarRun', description='Hello World DAG',
+  schedule_interval='0 0 1 * *',
+  start_date=datetime(2022, 5, 15), catchup=False) as dag:
   t1 = DummyOperator(task_id = "dummy")
 #   t2 = DummyOperator(task_id = "dummy2")
 
-  t2 = BashOperator(
-    task_id = 'jar_run', bash_command = 'java -cp scala-spark_2.13-0.1.0-SNAPSHOT.jar check1'
+  t3 = BashOperator(
+    task_id = 'directory_find', bash_command = 'cd ~/../../opt/airflow/dags/ && pwd && java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar TwitterToKafka'
   )
-  t1>>t2
+
+  # t4 = BashOperator(
+  #   task_id = 'directory_find', bash_command = 'pwd'
+  # )
+
+  # t2 = BashOperator(
+  #   task_id = 'jar_run', bash_command = 'java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar TwitterToKafka'
+  # )
+  t1>>t3
