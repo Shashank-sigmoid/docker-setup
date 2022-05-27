9c9961ad0a528cf7aae617c566d31ce51e0f9790
Shashank-sigmoid <shashank1@sigmoidanalytics.com>
2022-05-27 14:32:49 +0530

    all tasks added

--- dags/project.py
+++ dags/project.py
@@ -3,39 +3,43 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
 from pathlib import Path
+from airflow.operators.python import PythonOperator
 import os
+from query_6 import query_6
 
 PROJECT_ROOT = Path(__file__).parent.absolute()
 QUERIES_FILE = os.path.join(PROJECT_ROOT, "resources/keywords.txt")
 
-with DAG('JarRun', description = 'Hello World DAG',
+with DAG('ProjectRun', description='Hello World DAG',
 
-  schedule_interval = '0 0 1 * *',
+         schedule_interval='0 0 1 * *',
 
-  start_date=datetime(2022, 5, 15), catchup=False) as dag:
+         start_date=datetime(2022, 5, 25), catchup=False) as dag:
+    t1 = DummyOperator(task_id="Dummy")
 
-  t1 = DummyOperator(task_id = "dummy")
+    t2 = BashOperator(
+        task_id='TwitterToKafka',
+        env={"QUERIES_FILE": QUERIES_FILE},
+        bash_command='cd ~/../../opt/airflow/dags/ && pwd && java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar TwitterToKafka'
+    )
 
-  t2 = BashOperator(
-    task_id = 'TwitterToKafka',
-    env = {"QUERIES_FILE": QUERIES_FILE},
-    bash_command = 'cd ~/../../opt/airflow/dags/ && pwd && java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar TwitterToKafka'
-  )
+    t3 = BashOperator(
+        task_id='KafkaToMongo',
+        bash_command='cd ~/../../opt/airflow/dags/ && pwd && java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar KafkaToMongo'
+    )
 
-  t3 = BashOperator(
-    task_id = 'KafkaToMongo',
-    bash_command = 'cd ~/../../opt/airflow/dags/ && pwd && java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar KafkaToMongo'
-  )
+    t4 = BashOperator(
+        task_id='Query5_TwitterToKafka',
+        bash_command='cd ~/../../opt/airflow/dags/ && pwd && java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar Query5_TwitterToKafka'
+    )
 
-  t4 = BashOperator(
-    task_id = 'Query5_TwitterToKafka',
-    bash_command = 'cd ~/../../opt/airflow/dags/ && pwd && java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar Query5_TwitterToKafka'
-  )
+    t5 = BashOperator(
+        task_id='Query5_KafkaToMongo',
+        bash_command='cd ~/../../opt/airflow/dags/ && pwd && java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar Query5_KafkaToMongo'
+    )
 
-  t5 = BashOperator(
-    task_id = 'Query5_KafkaToMongo',
-    bash_command='cd ~/../../opt/airflow/dags/ && pwd && java -cp covid-tweet-analysis-assembly-0.1.0-SNAPSHOT.jar Query5_KafkaToMongo'
-  )
-
-  t1 >> t2 >> t3 >> t4 >> t5
-
+    t6 = PythonOperator(
+        task_id='Query_6',
+        python_callable=query_6
+    )
+    t1 >> t6
