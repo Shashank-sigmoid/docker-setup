7d851a9817f7eb25250616db2e25c415606249b2
Shashank-sigmoid <shashank1@sigmoidanalytics.com>
2022-06-10 14:44:52 +0530

    Converting Date format of twitter data from String to ISO

---
+++ dags/Summary_table/date_conversion.py
@@ -0,0 +1,44 @@
+from pymongo import MongoClient
+from dateutil.parser import parse
+
+
+def date_conversion():
+
+    try:
+        # Establishing connection with the MongoDB
+        client = MongoClient("mongodb://root:root@mongo:27017")
+        print("Connection established successfully...")
+
+        # Storing the database named "twitter_db" in variable mydb
+        mydb = client["twitter_db"]
+
+        # Storing the collection "covid_tweets" in a variable named coll
+        coll = mydb["covid_tweets"]
+
+        # Storing the collection "who_tweets" in a variable named coll1
+        coll1 = mydb["who_tweets"]
+
+        # Converting field "created_at" of collection "covid_tweets" from String to ISO Date format using dateutil
+        for doc in coll.find():
+            dtime = doc["created_at"]
+            new_datetime = parse(dtime)
+            my_query = {"created_at": doc["created_at"]}
+            new_value = {"$set": {"created_at": new_datetime}}
+            coll.update_one(my_query, new_value)
+
+        # Converting field "created_at" of collection "who_tweets" from String to ISO Date format using dateutil
+        for doc in coll1.find():
+            dtime = doc["created_at"]
+            new_datetime = parse(dtime)
+            my_query = {"created_at": doc["created_at"]}
+            new_value = {"$set": {"created_at": new_datetime}}
+            coll1.update_one(my_query, new_value)
+
+        print("Date converted successfully...")
+
+    except:
+        print("Error in the connection...")
+
+
+if __name__ == "__main__":
+    date_conversion()
